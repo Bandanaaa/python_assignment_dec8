7fbd7c5e3c100233914d156270e0057ef55a2584
Tejas <tejaspant55@gmail.com>
2017-12-11 12:38:58 +0545

    acceleration calculated

---
+++ password.py
@@ -0,0 +1,13 @@
+
+
+password= "assignment"
+
+def word(value):
+    if(value == password):
+        print("Password match")
+    else:
+        print("Wrong password")
+
+
+value=input("Enter the password: ")
+word(value)
